9b4778027e5da53f2af673f12ef23d726a36b31a
j-sauceda <jorge.sauceda@gmail.com>
2024-08-22 20:28:35 -0600

    Modified footer text

--- src/App.tsx
+++ src/App.tsx
@@ -156,7 +156,7 @@ const App = () => {
       </div>
       <div className="footer">
         <span>
-          Built by <a href="https://www.jsauceda.info">Jorge Sauceda, 2024</a>
+          Built by <a href="https://www.jsauceda.info">Jorge Sauceda</a> on 2024
         </span>
       </div>
     </div>
